4a9b6778c740c5fe80b635247870e8ba9e613bc5
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2021-02-25 14:09:23 +0000

    Remove non-functional helper

--- src/helpers.ts
+++
@@ -1,3 +0,0 @@
-export function late<T>(fn: () => T): T {
-  return fn();
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * as ops from './operators'
-export * from "./helpers";
 export * from "./interfaces";
 export * from "./jetpack";
 export * from "./machine";
